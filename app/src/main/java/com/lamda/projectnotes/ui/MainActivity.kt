9c7cd6c58fd6ab026fa0e91c988aba8f9c8a53fd
Jam <nail.djamal@gmail.com>
2023-04-10 21:40:53 +0100

    Added animation to the navigation graph

--- app/src/main/java/com/lamda/projectnotes/ui/MainActivity.kt
+++ app/src/main/java/com/lamda/projectnotes/ui/MainActivity.kt
@@ -3,20 +3,22 @@ package com.lamda.projectnotes.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.lamda.projectnotes.ui.theme.ProjectNotesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ProjectNotesTheme {
-                val navController: NavHostController = rememberNavController()
+                val navController: NavHostController = rememberAnimatedNavController()
                 NavGraph(navController = navController)
             }
         }
--- app/src/main/java/com/lamda/projectnotes/ui/NavGraph.kt
+++ app/src/main/java/com/lamda/projectnotes/ui/NavGraph.kt
@@ -1,11 +1,17 @@
 package com.lamda.projectnotes.ui
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+
+import com.google.accompanist.navigation.animation.composable
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.lamda.projectnotes.ui.category.ManageCategories
 import com.lamda.projectnotes.ui.create_update.CreateUpdateScreen
 import com.lamda.projectnotes.ui.home.HomeScreen
@@ -13,11 +19,12 @@ import com.lamda.projectnotes.ui.manage_note.ManageDeletedNotes
 import com.lamda.projectnotes.ui.manage_note.ManageNoteScreen
 
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NavGraph (
     navController: NavHostController
 ){
-    NavHost(
+    AnimatedNavHost(
         navController = navController,
         startDestination = AppDestinations.Home.route
     )
@@ -31,7 +38,9 @@ fun NavGraph (
             arguments = listOf(navArgument(name = "noteId") {
                 type = NavType.IntType
             }
-            )
+            ),
+            enterTransition = { scaleIn(tween(300)) },
+            exitTransition = { slideOutHorizontally (tween(400))}
         ) {
             ManageNoteScreen(navController = navController)
         }
@@ -40,15 +49,25 @@ fun NavGraph (
             arguments = listOf(navArgument(name = "noteId") {
                 type = NavType.IntType
             }
-            )
+            ),
+            enterTransition = { scaleIn(tween(300)) },
+            exitTransition = { slideOutHorizontally (tween(400))}
         ) {
             CreateUpdateScreen(navController = navController)
         }
-        composable(route = AppDestinations.ManageCategories.route)
+        composable(
+            route = AppDestinations.ManageCategories.route,
+            enterTransition = { slideInHorizontally(tween(300)) },
+            exitTransition = { slideOutHorizontally(tween(400))}
+        )
         {
             ManageCategories(navController = navController)
         }
-        composable(route = AppDestinations.ManageDeleted.route)
+        composable(
+            route = AppDestinations.ManageDeleted.route,
+            enterTransition = { slideInHorizontally(tween(300)) },
+            exitTransition = { slideOutHorizontally(tween(400))}
+        )
         {
             ManageDeletedNotes(navController = navController)
         }
